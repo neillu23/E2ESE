7f098c498a0ed28d0343819b603ed8ce9b106ab0
neillu23 <neilyenjulu@gmail.com>
2020-07-29 20:36:17 +0800

    First edition for generating fbank

--- PytorchSE/secode/CombinedModel.py
+++ PytorchSE/secode/CombinedModel.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import numpy
 
 class CombinedModel(nn.Module):
     def __init__(self, SEmodel, ASRmodel, criterion, alpha):
@@ -21,3 +22,31 @@ class CombinedModel(nn.Module):
         loss = SEloss + self.alpha * ASRloss
         return loss
 
+class Fbank(nn.Module):
+    def __init__(self,sample_rate=16000,NFFT=400,nfilt=26):
+        self.sample_rate = sample_rate
+        self.NFFT = NFFT
+        self.nfilt = nfilt
+
+    def forward(pow_frames):
+        low_freq_mel = 0
+        high_freq_mel = (2595 * numpy.log10(1 + (self.sample_rate / 2) / 700))  # Convert Hz to Mel
+        mel_points = numpy.linspace(low_freq_mel, high_freq_mel, nfilt + 2)  # Equally spaced in Mel scale
+        hz_points = (700 * (10**(mel_points / 2595) - 1))  # Convert Mel to Hz
+        bin = numpy.floor((self.NFFT + 1) * hz_points / self.sample_rate)
+
+        fbank = numpy.zeros((self.nfilt, int(numpy.floor(self.NFFT / 2 + 1))))
+        for m in range(1, self.nfilt + 1):
+            f_m_minus = int(bin[m - 1])   # left
+            f_m = int(bin[m])             # center
+            f_m_plus = int(bin[m + 1])    # right
+
+            for k in range(f_m_minus, f_m):
+                fbank[m - 1, k] = (k - bin[m - 1]) / (bin[m] - bin[m - 1])
+            for k in range(f_m, f_m_plus):
+                fbank[m - 1, k] = (bin[m + 1] - k) / (bin[m + 1] - bin[m])
+        #need to modify numpy.dot to torch
+        filter_banks = numpy.dot(pow_frames, fbank.T)
+        filter_banks = numpy.where(filter_banks == 0, numpy.finfo(float).eps, filter_banks)  # Numerical Stability
+        filter_banks = 20 * numpy.log10(filter_banks)  # dB
+        return filter_banks
