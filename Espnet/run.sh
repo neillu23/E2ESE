b2a142021127c4291c59cddc240aefdf50ab53a8
ChiaYu-Chang <r07522520@ntu.edu.tw>
2020-10-06 14:18:46 +0000

    Add espnet stage1&2

--- Espnet/run.sh
+++ Espnet/run.sh
@@ -1,8 +1,12 @@
-ESPNET_PATH="/mnt/Data/user_vol_2/user_neillu/End2End/espnet/egs/timit/asr_normwave_fbank/"
-CLEAN_TIMIT="/mnt/Data/user_vol_2/user_neillu/TIMIT_Manner_E2E/"
-DATA_PATH="/mnt/Data/user_vol_2/user_neillu/TIMIT_fbank/"
+ESPNET_PATH="/data1/user_chiayow/espnet/egs/timit/asr1"
+CLEAN_TIMIT="/data1/user_chiayow/data/TIMIT_Manner_E2E/"
+DATA_PATH="/data1/user_chiayow/data/TIMIT_fbank"
+PWD_PATH='/data1/user_chiayow/End2End/Espnet'
 
+cd $ESPNET_PATH
+./run.sh --stop_stage 2 
 
+cd $PWD_PATH/
 # step 1: generate normalized fbank
 mkdir out/
 python make_list.py  $CLEAN_TIMIT out/TIMIT_filelist.txt
@@ -13,9 +17,9 @@ copy-feats ark,t:$DATA_PATH/timit_fbank.txt ark,scp:$DATA_PATH/timit_fbank.ark,$
 python change_data.py $DATA_PATH/timit_fbank.scp $ESPNET_PATH/dump/test/deltafalse/data.json out/data_test.json
 python change_data.py $DATA_PATH/timit_fbank.scp $ESPNET_PATH/dump/train_dev/deltafalse/data.json out/data_train_dev.json
 python change_data.py $DATA_PATH/timit_fbank.scp $ESPNET_PATH/dump/train_nodev/deltafalse/data.json out/data_train_nodev.json
-cp out/data_test.json $ESPNET_PATH/dump/test/deltafalse/data.json
-cp out/data_train_dev.json $ESPNET_PATH/dump/train_dev/deltafalse/data.json 
-cp out/data_train_nodev.json $ESPNET_PATH/dump/train_nodev/deltafalse/data.json 
+cp out/data_test.json $ESPNET_PATH/dump/test/deltafalse/data.json ../PytorchSE/data/
+cp out/data_train_dev.json $ESPNET_PATH/dump/train_dev/deltafalse/data.json ../PytorchSE/data/
+cp out/data_train_nodev.json $ESPNET_PATH/dump/train_nodev/deltafalse/data.json ../PytorchSE/data/
 
 # step 3: train & test ASR model
 cd $ESPNET_PATH/
@@ -24,5 +28,5 @@ cd $ESPNET_PATH/
 # step 4: copy model from espnet and run SE-ASR
 cd -
 cd ../PytorchSE
-cp $ESPNET_PATH/exp/train_nodev_pytorch_train/results/model.loss.best.entire data/newctcloss.model.acc.best.entire.pth
+cp $ESPNET_PATH/exp/train_nodev_pytorch_train/results/model.acc.best.entire data/newctcloss.model.acc.best.entire.pth
 sh run.sh
