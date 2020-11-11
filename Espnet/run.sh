fe260a19376f8209b59057113aa9aa313dc38085
neillu23 <neilyenjulu@gmail.com>
2020-11-11 15:49:18 +0800

    update run.sh

--- Espnet/run.sh
+++ Espnet/run.sh
@@ -3,15 +3,22 @@ CLEAN_TIMIT="/data1/user_chiayow/data/TIMIT_Manner_E2E/"
 DATA_PATH="/data1/user_chiayow/data/TIMIT_fbank"
 PWD_PATH='/data1/user_chiayow/End2End/Espnet'
 
+E2E_PATH=$PWD
+
+# step 0: run the first time ESPnet
 cd $ESPNET_PATH
 ./run.sh --stop_stage 2 
 
-cd $PWD_PATH/
+cd $E2E_PATH
 # step 1: generate normalized fbank
 mkdir out/
 python make_list.py  $CLEAN_TIMIT out/TIMIT_filelist.txt
 python gen_fbank.py  out/TIMIT_filelist.txt $DATA_PATH/timit_fbank.txt
 copy-feats ark,t:$DATA_PATH/timit_fbank.txt ark,scp:$DATA_PATH/timit_fbank.ark,$DATA_PATH/timit_fbank.scp
+
+cp out/data_test.json ../PytorchSE/data/
+cp out/data_train_dev.json ../PytorchSE/data/
+cp out/data_train_nodev.json ../PytorchSE/data/
 
 # step 2: change data.json 
 python change_data.py $DATA_PATH/timit_fbank.scp $ESPNET_PATH/dump/test/deltafalse/data.json out/data_test.json
@@ -26,7 +33,7 @@ cd $ESPNET_PATH/
 ./run.sh --stage 3 
 
 # step 4: copy model from espnet and run SE-ASR
-cd -
+cd $E2E_PATH
 cd ../PytorchSE
 cp $ESPNET_PATH/exp/train_nodev_pytorch_train/results/model.acc.best.entire data/newctcloss.model.acc.best.entire.pth
 sh run.sh
--- PytorchSE/run.sh
+++ PytorchSE/run.sh
@@ -1,5 +1,5 @@
 
-stage=2
+stage=0
 
 # Input waveform data
 TRAIN_NOISY="/home/neillu/Desktop/Workspace/TIMIT_noisy_40hr_wav/TrNoisy/"
@@ -25,5 +25,10 @@ fi
 
 if [ ${stage} -le 2 ]; then
     echo "stage 2 : Testing"
-    python secode/main.py --mode 'test' --out_path $OUT_PATH --test_noisy $TEST_NOISY --test_clean $TEST_CLEAN
+    python secode/main.py --mode 'test' --out_path $OUT_PATH --test_noisy $TEST_NOISY --test_clean $TEST_CLEAN --after_alpha_epoch
+fi
+
+if [ ${stage} -le 3 ]; then
+    echo "stage 3 : Scoring"
+    python preprocess/cal_score.py $OUT_PATH/Result/transformerencoder_03_10000Normal_MAP_epochs150_adam_l1_alpha0.0_after_alpha_epoch0.0_batch4_lr5e-05.csv $OUT_PATH/Score/
 fi
